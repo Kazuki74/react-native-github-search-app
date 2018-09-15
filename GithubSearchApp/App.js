04bc836a290b36a15e044564538b277854a5c68d
小林和希 <kobayashi.kazuki74@gmail.com>
2018-09-15 20:02:06 +0900

    added fetch function and flatlist

--- GithubSearchApp/App.js
+++ GithubSearchApp/App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
 import {
   Platform,
@@ -13,21 +5,44 @@ import {
   Text,
   View,
   TouchableOpacity,
+  FlatList,
 } from 'react-native';
 
-type Props = {};
-export default class App extends Component<Props> {
-  onPressFetch() {
-    fetch('https://api.github.com/search/repositories?q=react&page=2')
+export default class App extends Component<{}> {
+
+  constructor() {
+    super();
+    this.state = {
+      items: [],
+      page: 0,
+    };
+  }
+  fetchRepositories() {
+    const newPage = this.state.page + 1;
+    const url = `https://api.github.com/search/repositories?q=react&page=${newPage}`
+    console.log(newPage)
+    fetch(url)
     .then(response => response.json())
-    .then(({items}) => console.log(items))
+    .then(({items}) => {
+        this.setState({ items: [...this.state.items, ...items], page: newPage})
+      }
+    )
   }
   render() {
+    console.log(this.state.items)
     return (
       <View style={styles.container}>
-        <TouchableOpacity onPress={this.onPressFetch}>
+        <TouchableOpacity onPress={() => this.fetchRepositories()}>
           <Text>Fetch</Text>
         </TouchableOpacity>
+        <FlatList
+          data={this.state.items}
+          renderItem={({item, index}) => <Text style={styles.text}>{item.name}</Text>}
+          keyExtractor={(item) => `${item.id}`}
+          onEndReached={() => this.fetchRepositories()}
+          onEndReachedThreshold={0.1}
+        >
+        </FlatList>
       </View>
     );
   }
@@ -36,8 +51,10 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    marginTop: 20,
   },
+  text: {
+    margin: 20,
+  }
 });

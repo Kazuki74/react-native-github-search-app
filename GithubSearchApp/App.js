2908c3e68bc9a089763db7d5391300316f5ee82b
小林和希 <kobayashi.kazuki74@gmail.com>
2018-09-15 20:16:34 +0900

    added pullRefresh function

--- GithubSearchApp/App.js
+++ GithubSearchApp/App.js
@@ -15,21 +15,25 @@ export default class App extends Component<{}> {
     this.state = {
       items: [],
       page: 0,
+      refreshing: false,
     };
   }
-  fetchRepositories() {
-    const newPage = this.state.page + 1;
+  fetchRepositories(refreshing = false) {
+    const newPage = refreshing ? 1 : this.state.page + 1;
     const url = `https://api.github.com/search/repositories?q=react&page=${newPage}`
     console.log(newPage)
     fetch(url)
     .then(response => response.json())
     .then(({items}) => {
-        this.setState({ items: [...this.state.items, ...items], page: newPage})
+      if (refreshing){
+        this.setState({ items, refreshing:false });
+      } else {
+        this.setState({ items: [...this.state.items, ...items], page: newPage, refreshing:false })
+      }
       }
     )
   }
   render() {
-    console.log(this.state.items)
     return (
       <View style={styles.container}>
         <TouchableOpacity onPress={() => this.fetchRepositories()}>
@@ -37,10 +41,12 @@ export default class App extends Component<{}> {
         </TouchableOpacity>
         <FlatList
           data={this.state.items}
-          renderItem={({item, index}) => <Text style={styles.text}>{item.name}</Text>}
+          renderItem={({item, index}) => <Text style={styles.text}>{item.full_name}</Text>}
           keyExtractor={(item) => `${item.id}`}
           onEndReached={() => this.fetchRepositories()}
           onEndReachedThreshold={0.1}
+          onRefresh={() => {this.fetchRepositories(true)}}
+          refreshing={this.state.refreshing}
         >
         </FlatList>
       </View>
